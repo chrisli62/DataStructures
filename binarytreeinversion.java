097b493ced5f30ba2113cdf754b8081c6045af75
chrisli62 <86215173+chrisli62@users.noreply.github.com>
2025-07-03 18:25:49 -0400

    Update binarytreeinversion.java

--- binarytreeinversion.java
+++ binarytreeinversion.java
@@ -1,11 +1,11 @@
 //Time complexity: O(n)
-    //Space complexity: O(nm)
-    public TreeNode invertTree(TreeNode root) {
-        if(root == null) return root;
+//Space complexity: O(nm)
+public TreeNode invertTree(TreeNode root) {
+    if(root == null) return root;
         
-        TreeNode temp = root.left;
-        root.left = invertTree(root.right);
-        root.right = invertTree(temp);
+    TreeNode temp = root.left;
+    root.left = invertTree(root.right);
+    root.right = invertTree(temp);
         
-        return root;
-    }
+    return root;
+}
